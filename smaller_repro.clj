7095d10b59bd22d37de873667d9270004b0da5b4
Peter Strömberg <pez@pezius.com>
2023-10-20 22:43:49 +0200

    Add smaller repro

---
+++ smaller_repro.clj
@@ -0,0 +1,8 @@
+;; Some experience erratic typing in this file too, even if less complete lockup
+(ns yamlscript.re)
+(def xxx #"(?x))")
+(def xxx
+  {:xxx x
+         ()"
+        (let [xxx (xxx (str #"\$" #"(?!\w)"))]
+      s xxx)))
